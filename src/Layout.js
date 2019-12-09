c74c58cd4bb68039aea218347cdab96370d78c3b
Mikael Hinton <43019867+Hnton@users.noreply.github.com>
2019-12-08 22:48:57 -0500

    ed

---
+++ src/Layout.js
@@ -0,0 +1,31 @@
+import React from "react";
+import type { Node } from "react";
+import { Link } from "@reach/router";
+
+type Props = {
+  children: Node
+};
+const h4Style = {
+  display: "inline-block",
+  marginRight: "1em"
+};
+
+export default ({ children }: Props) => {
+  return (
+    <div>
+      <h4 style={h4Style}>
+        <Link to="/Home">Home</Link>
+      </h4>
+      <h4 style={h4Style}>
+        <Link to="/brian">Brian</Link>
+      </h4>
+      <h4 style={h4Style}>
+        <Link to="/hinton">Hinton</Link>
+      </h4>
+      <h4 style={h4Style}>
+        <Link to="/jeremii">@Jeremii</Link>
+      </h4>
+      <main>{children}</main>
+    </div>
+  );
+};
--- src/pages/Jeremii.js
+++ src/pages/Jeremii.js
@@ -244,4 +244,3 @@ function Jeremii() {
 }
 
 export default Jeremii;
-  
